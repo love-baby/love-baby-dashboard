a2d5e6e62f589fdab9f29ea8d98a78bd0d51d33f
liangbc <bicong.liang@51zouchuqu.com>
2018-07-11 17:04:14 +0800

    初始化

--- src/main/java/com/love/baby/dashboard/DashboardApplication.java
+++ src/main/java/com/love/baby/dashboard/DashboardApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 @EnableDiscoveryClient
 @SpringCloudApplication
 @EnableHystrixDashboard
+@EnableTurbine
 @Controller
 public class DashboardApplication {
 
